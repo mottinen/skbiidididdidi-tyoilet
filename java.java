3acd4e077cec959389213d766030457d2498cf93
mottinen <tom.lostedt@gmail.com>
2024-09-27 09:11:16 +0300

    Add files via upload

---
+++ java.java
@@ -0,0 +1,12 @@
+public class java {
+    
+    public static void main(string args[]) {
+        int abc = 20;
+        if(abc < 18) {
+            system.out.PrintIn("abc is lesser");
+        }else{ 
+            system.out.PrintIn("abc is greater");
+        }
+            
+}
+ 
